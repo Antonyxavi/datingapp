3f398b12db8f528bd6e50eae9f0ecb60d60b5bd4
Antonyxavi <antonyxav9@gmail.com>
2021-07-11 12:30:43 +0530

    End of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit{
+  model:any={}
+  loggedIn: boolean | undefined;
+
+  constructor(private accountService:AccountService)
+  {
+
+  }
+
+  ngOnInit(): void {
+    this.getCurrentUer();
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe(response =>{
+      console.log(response);
+      this.loggedIn=true;
+    },error =>
+    console.log(error));
+  }
+
+  logout()
+  {
+    this.accountService.logout();
+    this.loggedIn=false;
+  }
+
+  getCurrentUer()
+  {
+    this.accountService.currentUser$.subscribe(user =>{
+      this.loggedIn=!!user;
+    },error=>{
+      console.log(error);
+    })
+  }
+
+}
