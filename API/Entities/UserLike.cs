161f4059110062c28a65cb6eebd599f0d063ec65
Antonyxavi <antonyxav9@gmail.com>
2021-07-26 02:07:08 +0530

    End of section 14

---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,13 @@
+namespace API.Entities
+{
+    public class UserLike
+    {
+        public Appuser SourceUser { get; set; }
+
+        public int SourceUserId { get; set; }
+
+        public Appuser LikedUser { get; set; }
+
+        public int LikedUserId { get; set; }
+    }
+}
