03b82e8e0e6a6949986126bd552e964aa019e797
Antonyxavi <antonyxav9@gmail.com>
2021-07-30 01:25:50 +0530

    End of section 17

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -5,6 +5,7 @@ import {map} from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 import { User } from '../_models/user';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class AccountService {
   private currentUserSource=new ReplaySubject<User>(1);
   currentUser$=this.currentUserSource.asObservable();
 
-  constructor(private http:HttpClient) {}
+  constructor(private http:HttpClient, private presence:PresenceService) {}
 
   login(model:any)
   {
@@ -23,6 +24,7 @@ export class AccountService {
         const user=response;
         if(user){
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
       })
     )
@@ -34,6 +36,7 @@ register(model:any)
      map((user:User)=>{
        if(user){
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
        }
      })
   )
@@ -56,6 +59,7 @@ setCurrentUser(user: User) {
     localStorage.removeItem('user');
   //this.currentUserSource = JSON.parse(localStorage.getItem('user') || '{}');
     this.currentUserSource.next(null!);
+    this.presence.stopHUbConnection();
   }
 
   getDecodedToken(token)
